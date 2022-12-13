60930d77739d4c17f14c74177d0807f0773b50d9
NagyeongHa <hnk3176@naver.com>
2022-12-13 17:15:50 +0900

    Set: backendHost 변경

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,8 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080/uriharu";
 } else {
-  backendHost = "https://uri-haru.herokuapp.com";
+  backendHost =
+    "https://port-0-uriharu-backend-422t024lblxt1w1.gksl2.cloudtype.app/";
 }
 
 export const API_BASE_URL = `${backendHost}`;
